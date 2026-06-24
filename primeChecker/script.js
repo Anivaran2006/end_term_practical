6110d8419d3bc8b64d8383f57d8cac7860f5176b
Anivaran Dubey <aniv8007@gmail.com>
2026-06-24 11:37:52 +0530

    new

--- primeChecker/script.js
+++ primeChecker/script.js
@@ -1,26 +1,30 @@
 function checkPrime() {
 
-    let num = document.getElementById("num").value;
+    let num = Number(document.getElementById("num").value);
     let prime = true;
 
-    if(num <= 1){
+    if (num < 0) {
+        document.getElementById("result").innerHTML =
+            "Error: Prime numbers do not exist for negative numbers";
+        return;
+    }
+
+    if (num <= 1) {
         prime = false;
     }
 
-    for(let i = 2; i < num; i++){
-
-        if(num % i == 0){
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0) {
             prime = false;
             break;
         }
     }
 
-    if(prime){
+    if (prime) {
         document.getElementById("result").innerHTML =
-        num + " is a Prime Number";
-    }
-    else{
+            num + " is a Prime Number";
+    } else {
         document.getElementById("result").innerHTML =
-        num + " is Not a Prime Number";
+            num + " is Not a Prime Number";
     }
 }
